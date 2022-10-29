30f936f649e85b3a2a4d62fb5da2e38e1fca3822
Rayyan_ <omg4twp@gmail.com>
2022-10-29 22:25:37 +0500

    Hex to Oct
    Added the functionality to convert Oct into Hex by first asking for Hex values and then converting the Hex digits like "F" from strings to integers using int(Hex, 16)

---
+++ hexadecimal/hex-dec.py
@@ -0,0 +1,7 @@
+#Initializing Hex Value
+Hex = input("Input Hex Value: ")
+
+#Finding out Octal Value
+Oct = int(Hex, 16) #Hex = Hex, 16= Hex base
+str(Oct) #So the result is in one line ig
+print(Oct)
