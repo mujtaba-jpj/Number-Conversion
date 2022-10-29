645061cf710def51f1b5c578f4437255d17b7c36
mujtabafturmom <73431294+mujtaba-jpj@users.noreply.github.com>
2022-10-29 22:32:03 +0500

    Fixed hex to dec

---
+++ hexadecimal/hex-bin.py
@@ -0,0 +1,5 @@
+def hex_bin(hex):
+    bin = int(hex,16    )
+    print(bin)
+
+hex_bin("1a")
--- hexadecimal/hex-dec.py
+++ hexadecimal/hex-dec.py
@@ -1,7 +1,6 @@
-#Initializing Hex Value
-Hex = input("Input Hex Value: ")
+def hex_dec(hex):
+    #Initializing Hex Value
 
-#Finding out Octal Value
-Oct = int(Hex, 16) #Hex = Hex, 16= Hex base
-str(Oct) #So the result is in one line ig
-print(Oct)
+    #Finding out Octal Value
+    decimal = int(hex, 16) #Hex = Hex, 16= Hex base
+    print(decimal)
