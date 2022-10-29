79dc3f3f9bbf588a7aea6a16a3d98b127507384d
mujtabafturmom <73431294+mujtaba-jpj@users.noreply.github.com>
2022-10-29 22:07:53 +0500

    fixed the output of functions

--- Binary/bin-hex.py
+++ Binary/bin-hex.py
@@ -3,8 +3,7 @@ from bindec import bin_dec
 def bin_hex(bin):
     decimal = bin_dec(bin)
     hexadecimal = hex(decimal)  
-    hexadecimal = hexadecimal.split('x')
-    print(hexadecimal[1])
+    hexadecimal = hexadecimal.split('x')[1]
     return hexadecimal
 
 
--- Binary/bin-octal.py
+++ Binary/bin-octal.py
@@ -3,6 +3,6 @@ from bindec import bin_dec
 def bin_oct(bin):
     decimal = bin_dec(bin)
     octal = oct(decimal)
-    octal = octal.split('o')
+    octal = octal.split('o')[1]
     return octal
 
