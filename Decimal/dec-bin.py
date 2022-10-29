19d89aea77f52a90cd56c2140eec563f577e8524
mujtabafturmom <73431294+mujtaba-jpj@users.noreply.github.com>
2022-10-29 22:05:36 +0500

    Added decimal to binary and hexadecimal

--- Decimal/dec-bin.py
+++ Decimal/dec-bin.py
@@ -1,8 +1,8 @@
-#Initialzing and Assigning Decimal
-Decimal = bin(int(input("enter a num: ")))
+def dec_bin(decimal):
+    #Initialzing and Assigning Decimal
+    binary = bin(decimal)
 
-#Splitting the string in 2 parts: 0b, 11101000
-FixedDecimal = Decimal.split("0b")
+    #Splitting the string in 2 parts: 0b, 11101000
+    binary = binary.split("b")[1]
+    return binary
 
-#Print the 1 part of string i.e 111010000 and ignore the 0 port i.e 0b
-print(Decimal[1])
---
+++ Decimal/dec-hex.py
@@ -0,0 +1,4 @@
+def dec_hex(decimal):
+    hexadecimal = hex(decimal)
+    hexadecimal = hexadecimal.split("x")[1]
+    return hexadecimal
